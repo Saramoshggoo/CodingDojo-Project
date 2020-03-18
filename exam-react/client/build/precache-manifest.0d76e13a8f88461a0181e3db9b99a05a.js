9f7c37e346c901ec62ab101ecbcb2874e9e843df
Saramoshggoo <smoshggoo@gmail.com>
2020-03-18 15:04:25 -0700

    first

---
+++ exam-react/client/build/precache-manifest.0d76e13a8f88461a0181e3db9b99a05a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c27a8e5a731e70e80bf1aff81adaeb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e931e04f785ca9da4e",
+    "url": "/static/css/main.ca03d7ed.chunk.css"
+  },
+  {
+    "revision": "2978c0ed975b0ce9805d",
+    "url": "/static/js/2.a5b19e94.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a5b19e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e931e04f785ca9da4e",
+    "url": "/static/js/main.72921f1a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
